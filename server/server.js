1dbc9c09f421ad37b3b95f18fec0b27f907ef75e
Aditya Bhattacharjee <adityab76@gmail.com>
2024-05-06 16:12:12 +0530

    updated code for deployment

--- server/server.js
+++ server/server.js
@@ -55,13 +55,22 @@ app.use(cors(
 ));
 app.use(express.json());
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 
 
-
-app.get('/api/',(req,res)=>{
-    return res.json("Hello user")
-})
 
 
 app.post('/api/Signup', async (req, res) => {
